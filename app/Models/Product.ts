4b6f61bb963dc04f6817d1cf098d079d555cf1df
Arthur Rezende <arthur-er@pm.me>
2022-06-26 11:25:35 -0300

    feat: category and product models

---
+++ app/Models/Product.ts
@@ -0,0 +1,32 @@
+import { DateTime } from 'luxon'
+
+import { attachment, AttachmentContract } from '@ioc:Adonis/Addons/AttachmentLite'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+
+import Category from './Category'
+
+export default class Product extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public active: boolean
+
+  @column()
+  public categoryId: number
+
+  @belongsTo(() => Category)
+  public category: BelongsTo<typeof Category>
+
+  @attachment()
+  public image: AttachmentContract
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
