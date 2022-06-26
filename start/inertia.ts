3303aa4116289afb228625e99fb0232aa2a8f5a9
Arthur Rezende <arthur-er@pm.me>
2022-06-26 15:44:40 -0300

    feat: share notifications to frontend

--- start/inertia.ts
+++ start/inertia.ts
@@ -11,6 +11,7 @@
 import Inertia from '@ioc:EidelLev/Inertia'
 
 Inertia.share({
-  errors: (ctx) => ctx.session.flashMessages.get('errors'),
-  user: (ctx) => ctx.auth.user,
+  errors: ({ session }) => session.flashMessages.get('errors'),
+  notifications: ({ session }) => session.flashMessages.get('notifications'),
+  user: ({ auth }) => auth.user,
 }).version(() => Inertia.manifestFile('public/assets/manifest.json'))
